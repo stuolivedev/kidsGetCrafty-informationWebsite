d7f223840fc8ab78f7b6b048a5f274f917e1ab95
Stu Olive <60480020+stuolivedev@users.noreply.github.com>
2020-09-28 12:28:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+$( document ).ready(function() {
+  $('#aboutBar').hide();
+  $('#pricesBar').hide();
+  $('#contactBar').show();
+  $('#galleryBar').hide();
+});
+
+$('#about').click(function(){
+  $('#aboutBar').show();
+  $('#pricesBar').hide();
+  $('#contactBar').hide();
+  $('#galleryBar').hide();
+})
+
+$('#prices').click(function(){
+  $('#aboutBar').hide();
+  $('#pricesBar').show();
+  $('#contactBar').hide();
+  $('#galleryBar').hide();
+})
+
+$('#contact').click(function(){
+  $('#aboutBar').hide();
+  $('#pricesBar').hide();
+  $('#contactBar').show();
+  $('#galleryBar').hide();
+})
 
 
 
@@ -7,9 +34,9 @@ $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() {
   $('#slideshow > div:first')
-    .fadeOut(1000)
+    .fadeOut(2000)
     .next()
-    .fadeIn(1000)
+    .fadeIn(2000)
     .end()
     .appendTo('#slideshow');
 }, 3000);
